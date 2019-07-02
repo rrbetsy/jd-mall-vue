b6f45d232e141fe722dbbef63dcdb6ad8b28679d
rrbetsy <1127667434@qq.com>
2019-07-02 22:26:23 +0800

    add category page

--- src/pages/category/Category.vue
+++ src/pages/category/Category.vue
@@ -1,35 +1,48 @@
 <template>
     <div class="category">
         <CategorySearchbar></CategorySearchbar>
-        <nut-tab
-            @tab-switch="tabSwitch"
-            positionNav="left"
-        >
-            <nut-tab-panel
-                v-for="category in categoryData"
-                :key="category.areaName"
-                :tabTitle="category.areaName"
-                v-html="category.content"
-            ></nut-tab-panel>
-        </nut-tab>
+        <div class="content-wrap">
+            <div class="left">
+                <ul>
+                    <li
+                        :class="['item',index ===activeIndex?'active':'']"
+                        v-for="(categoryItem,index) in categoryDatas"
+                        :key="categoryItem.areaName"
+                        @click="tabClick(index)"
+                    >{{categoryItem.areaName}}</li>
+                </ul>
+            </div>
+            <div class="right">
+                <CategoryList
+                    :active="dataIndex ===activeIndex"
+                    v-for="(categoryData,dataIndex) in categoryDatas"
+                    :key="`dt${dataIndex}`"
+                    :keywordList="categoryData.level1words"
+                ></CategoryList>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script>
 import './index.less'
 import data from './data/data'
 import CategorySearchbar from './CategorySearchbar'
+import CategoryList from './CategoryList'
 export default {
     name: 'Category',
     components: {
-        CategorySearchbar
+        CategorySearchbar,
+        CategoryList
     },
     mounted: function () {
-        console.log(data.keywordAreas)
+        // console.log(data.keywordAreas)
     },
     data () {
         return {
-            categoryData: data.keywordAreas,
+            categoryDatas: data.keywordAreas,
+            activeIndex: 0,
             positionNavCurr: "top",
             editableTabs: [
                 {
@@ -89,13 +102,49 @@ export default {
     methods: {
         tabSwitch: function (index, event) {
             console.log(index + "--" + event.target);
+        }, tabClick (index) {
+            this.activeIndex = index
         }
     }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang='less'>
 .category {
-    padding-top: 44px;
+    .content-wrap {
+        display: flex;
+        overflow: hidden;
+        height: 100vh;
+        padding-top: 44px;
+        box-sizing: border-box;
+        .left {
+            width: 85px;
+            position: relative;
+            overflow: hidden;
+            overflow-y: scroll;
+            -webkit-overflow-scrolling: touch;
+            ul {
+                li {
+                    height: 46px;
+                    line-height: 46px;
+                    text-align: center;
+                    color: #333;
+                    font-size: 14px;
+                    &.active {
+                        background: #fff;
+                        color: #e93b3d;
+                    }
+                }
+            }
+            &::-webkit-scrollbar {
+                display: none;
+            }
+        }
+        .right {
+            flex: 1;
+            background: #fff;
+            overflow-y: scroll;
+        }
+    }
 }
 </style>
---
+++ src/pages/category/CategoryList.vue
@@ -0,0 +1,95 @@
+<template>
+    <div
+        v-if="active"
+        :class="['category-list']"
+    >
+        <div
+            v-for="subItemLists in keywordList"
+            :key="subItemLists.keyword"
+        >
+            <div class="lv2-title">
+                {{subItemLists.keyword}}
+            </div>
+            <ul class="sub-item-list">
+                <li
+                    class="sub-item"
+                    v-for="(subItem,subItemIndex) in subItemLists.level2words"
+                    :key="`sub-item${subItemIndex}`"
+                >
+                    <div class="item-wrap">
+                        <transition name='fade'>
+                            <img
+                                v-if="active"
+                                :src="subItem.imageUrl"
+                                alt=""
+                            >
+                        </transition>
+                        <span>{{subItem.keyword}}</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryList',
+    props: {
+        keywordList: {
+            type: Array,
+            default: []
+        },
+        active: {
+            type: Boolean,
+            default: false
+        }
+    },
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='less'>
+.category-list {
+    // display: none;
+    .lv2-title {
+        padding: 15px 0 15px 15px;
+        font-weight: 700;
+    }
+    .sub-item-list {
+        display: flex;
+        flex-wrap: wrap;
+        .sub-item {
+            width: 32.8%;
+            overflow: hidden;
+            .item-wrap {
+                display: flex;
+                flex-direction: column;
+                text-align: center;
+                align-items: center;
+            }
+            img {
+                width: 70px;
+                height: 70px;
+                animation: imgShow .3s ease-in-out;
+                // opacity: 1;
+            }
+
+            span {
+                color: #333;
+                height: 35px;
+                margin-top: 5px;
+                -webkit-box-pack: start;
+                font-size: 12px;
+                color: #686868;
+                width: 100%;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
+    }
+}
+// .active-data {
+//     // display: block;
+// }
+</style>
