aece02d4dd963f3367eede1026cd3e55a4cf1afa
rrbetsy <1127667434@qq.com>
2019-07-31 08:53:45 +0800

    fix assets

--- src/pages/shop-cart/ProductInfo.vue
+++ src/pages/shop-cart/ProductInfo.vue
@@ -81,7 +81,55 @@ export default {
             actionSheetShow: {
             },
             showDetail: false,
-            productItems: [],
+            productItems: [{
+                imgUrl:
+                    '//img10.360buyimg.com/mobilecms/s117x117_jfs/t27616/251/1425719819/224805/20c2401e/5bc831fdN61f8d9d2.jpg!q70.dpg.webp',
+                name:
+                    'vivo Z3 6GB+64GB 极光蓝 性能实力派 全面屏游戏手机 移动联通电信全网通4G手机',
+                attr: '0.425kg/件，极光蓝，6GB+64GB',
+                price: 1248,
+                selected: false,
+                selectNum: 0
+            },
+            {
+                imgUrl:
+                    '//img10.360buyimg.com/n4/jfs/t1/58748/3/4030/192293/5d1c96fdE48ab9ad9/f7300d8229815101.jpg',
+                name:
+                    'Apple iPad 平板电脑 2018年新款9.7英寸（128G WLAN版/A10 芯片/Retina显示屏 MR7K2CH/A）银色',
+                attr: '0.84kg/件，银色，WIFI版128G',
+                price: 3177,
+                selectNum: 0,
+                selected: false
+            },
+            {
+                imgUrl:
+                    '//img10.360buyimg.com/mobilecms/s117x117_jfs/t1/29213/11/14211/274689/5ca56202Ec28b53c8/7fe5eb4a5571177d.jpg!q70.dpg.webp',
+                name: '城南旧事',
+                attr: '城南旧事',
+                selectNum: 0,
+                selected: false,
+                price: 31.8
+            },
+            {
+                imgUrl:
+                    '//img10.360buyimg.com/mobilecms/s117x117_jfs/t22720/73/1034241904/151911/8d39c1a7/5b4da4fdN7639b927.jpg!q70.dpg.webp',
+                name: '浮生六记（无删减彩色插图珍藏本）',
+                attr: '0.3kg/件，浮生六记',
+                selectNum: 0,
+                selected: false,
+                price: 34.8
+            },
+            {
+                imgUrl:
+                    '//img10.360buyimg.com/mobilecms/s117x117_jfs/t1/33790/26/4365/156233/5cb70b7dE61f2a717/5c0fc02565770b6b.jpg!q70.dpg.webp',
+                name:
+                    '原色派 苹果ipad pro 10.5保护套蓝牙键盘带笔槽新款ipad2018键盘air2皮套9.7 【钢琴灰皮套+白色键盘】 2018新ipad/pro9.7/air1/2通用',
+                attr:
+                    '【钢琴灰皮套+白色键盘】，2018新ipad/pro9.7/air1/2通用',
+                selectNum: 0,
+                selected: false,
+                price: 128
+            }],
             selectItems: {
                 // 所有选中的内容
             }
@@ -149,18 +197,18 @@ export default {
             this.$emit('update:totalNum', totalNum)
         }
     },
-    async created () {
-        try {
-            const { data } = await axios.get('http://localhost:3000/shop-cart/all');
-            // const {data} = await axios.get('http://localhost:3000/shop-cart/all');
-            if (data.code === 200) {
-                this.productItems = data.data;
-            }
-            console.log('cartData', data)
-        } catch (e) {
-            console.log(e)
-        }
-    }
+    // async created () {
+    //     try {
+    //         const { data } = await axios.get('http://localhost:3000/shop-cart/all');
+    //         // const {data} = await axios.get('http://localhost:3000/shop-cart/all');
+    //         if (data.code === 200) {
+    //             this.productItems = data.data;
+    //         }
+    //         console.log('cartData', data)
+    //     } catch (e) {
+    //         console.log(e)
+    //     }
+    // }
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
